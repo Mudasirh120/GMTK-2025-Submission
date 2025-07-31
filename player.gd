964c68b106a5d247aa99500b34eac7506b37fd58
Mudasirh120 <mudasirh120@gmail.com>
2025-07-31 13:41:57 +0500

    Player stealth and run implemented

--- player.gd
+++ player.gd
@@ -1,26 +1,32 @@
 extends CharacterBody2D
 @onready var timer:Timer = $Timer
-@onready var label:Label = $Label
+@onready var StaminaText:Label = $Stamina
+@onready var TotalSpeed:Label = $TotalSpeed
 const SPEED:int=100
 var Health:int= 100
-var isRunning: bool = false
+var isRunning: bool
 var Stamina :float= 1.0
-var totalCommulativeSpeed=1
-
+var totalCommulativeSpeed=1 	
+func _ready() -> void:
+	isRunning=false
+	timer.start()
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left",'move_right','move_forward',"move_backward")
-	timer.start()
+	print(direction)
 	if Input.is_action_pressed("run"):
-		isRunning=true
-		totalCommulativeSpeed = (SPEED * 2) * Stamina
+		if direction != Vector2():
+			isRunning=true
+			totalCommulativeSpeed = (SPEED * 2) * Stamina
 	elif Input.is_action_pressed('sneak'):
-		totalCommulativeSpeed = SPEED * 0.5
+		totalCommulativeSpeed = SPEED * 0.3
 	else:
+		isRunning=false
 		totalCommulativeSpeed = SPEED 
 	velocity= direction*totalCommulativeSpeed
-	isRunning=false
 	move_and_slide()
-	label.text=str(Stamina)
+	StaminaText.text=str(Stamina)
+	TotalSpeed.text=str(totalCommulativeSpeed)
+	
 
 
 func _on_timer_timeout() -> void:
@@ -28,4 +34,4 @@ func _on_timer_timeout() -> void:
 		Stamina-=0.1
 	else:
 		Stamina+=0.1
-	Stamina=clamp(Stamina,0.1,1)
+	Stamina=clamp(Stamina,0.5,1)
