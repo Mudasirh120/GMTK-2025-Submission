fb2a9e852bde1fcade893b0e336c08b4b959fe23
Mudasirh120 <mudasirh120@gmail.com>
2025-07-31 15:53:37 +0500

    Surrender logic and areas for attack and damage added for player

--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
-@onready var timer:Timer = $Timer
+@onready var RunningTimer:Timer = $RunningTimer
 @onready var StaminaText:Label = $Stamina
+@onready var HealthText:Label = $Health
 @onready var TotalSpeed:Label = $TotalSpeed
 const SPEED:int=100
 var Health:int= 100
@@ -8,35 +9,36 @@ var isRunning: bool
 var Stamina :float= 1.0
 var totalCommulativeSpeed=1
 var dodgeDashAvailable:bool
+var isSurrendered:bool=false
 func _ready() -> void:
 	isRunning=false
 	dodgeDashAvailable= true
-	timer.start()
+	RunningTimer.start()
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("move_left",'move_right','move_forward',"move_backward")
-	print(direction)
-	if Input.is_action_pressed("run"):
-		if direction != Vector2():
-			isRunning=true
-			totalCommulativeSpeed = (SPEED * 2) * Stamina
-	elif Input.is_action_pressed('sneak'):
-		totalCommulativeSpeed = SPEED * 0.3
-	elif Input.is_action_just_released("dash"):
-		if dodgeDashAvailable:
-			dodgeDashAvailable=false
-			totalCommulativeSpeed = (SPEED * 60)
-			Stamina-=0.6
-	else:
-		isRunning=false
-		totalCommulativeSpeed = SPEED 
-	velocity= direction*totalCommulativeSpeed
-	move_and_slide()
-	StaminaText.text=str(Stamina)
-	TotalSpeed.text=str(totalCommulativeSpeed)
-	
+	if !isSurrendered:
+		var direction = Input.get_vector("move_left",'move_right','move_forward',"move_backward")
+		if Input.is_action_pressed("run"):
+			if direction != Vector2():
+				isRunning=true
+				totalCommulativeSpeed = (SPEED * 2) * Stamina
+		elif Input.is_action_pressed('sneak'):
+			totalCommulativeSpeed = SPEED * 0.3
+		elif Input.is_action_just_released("dash"):
+			if dodgeDashAvailable:
+				dodgeDashAvailable=false
+				dodgeDash()
+		else:
+			isRunning=false
+			totalCommulativeSpeed = SPEED 
+		velocity= direction*totalCommulativeSpeed
+		StaminaText.text=str(Stamina)
+		TotalSpeed.text=str(totalCommulativeSpeed)
+		HealthText.text=str(Health)
+		if(Health <50):
+			Surrender()
+		move_and_slide()
 
-
-func _on_timer_timeout() -> void:
+func _on_running_timer_timeout() -> void:
 	if isRunning:
 		Stamina-=0.1
 	else:
@@ -44,3 +46,10 @@ func _on_timer_timeout() -> void:
 	Stamina=clamp(Stamina,0.5,1)
 	if Stamina==1.0:
 		dodgeDashAvailable=true
+func Surrender():
+	totalCommulativeSpeed=0
+	isSurrendered=true
+
+func dodgeDash():
+		totalCommulativeSpeed=SPEED*100
+		Stamina-=0.6
