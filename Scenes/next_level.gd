5134286c19eceb36a13694c1d64ddbaa53f41e2d
Mudasirh120 <mudasirh120@gmail.com>
2025-07-31 21:57:50 +0500

    Level Changing Option

---
+++ Scenes/next_level.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const BASE_PATH = 'res://Levels/level_'
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		var currentFile = get_tree().current_scene.scene_file_path
+		print(currentFile)
+		var nextLevel = currentFile.to_int()+1
+		print(nextLevel)
+		var nextLevelFile=BASE_PATH+str(nextLevel)+".tscn"
+		print(nextLevelFile)
+		get_tree().change_scene_to_file(nextLevelFile)
