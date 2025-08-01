8890ad6fb883ca2388c4637f708625918ff236eb
Hamza Butt <hb979067@gmail.com>
2025-08-01 16:17:21 +0500

    interact is not working currently might del

---
+++ Scripts/interact.gd
@@ -0,0 +1,17 @@
+extends RayCast2D
+@onready var player:CharacterBody2D=get_parent().get_parent().get_node("Player")
+@onready var ray:RayCast2D=$interactable
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_released("interact"):
+		ray.target_position=player.global_position+Vector2(0,100)
+		print(ray.target_position)
+	pass
+func attackdir():
+	if player.direction.y>0:
+		ray.rotation=0
+	elif player.direction.y<0:
+		ray.rotation=180
+	elif player.direction.x<0:
+		ray.rotation=-90
+	elif player.direction.x>0:
+		ray.rotation=90
