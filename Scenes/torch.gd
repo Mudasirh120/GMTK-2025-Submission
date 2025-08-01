2f572de6ffd04c9268c1140b37834b6a9ba4337d
Hamza Butt <hb979067@gmail.com>
2025-08-02 01:39:00 +0500

    ..

--- Scenes/torch.gd
+++ Scenes/torch.gd
@@ -1,12 +1,15 @@
 extends PointLight2D
 @onready var enem:CharacterBody2D=get_parent().get_node("Enemy")
-func _physics_process(delta: float) -> void:
-	
+@onready var torch:PointLight2D=get_parent().get_node("Torch")
+@onready var area:Area2D=$Area2D
+func _physics_process(delta: float):
+	if torch.disabled:
+		enem.lastSeenPos=area.global_position
+		enem.soun.disabled
 
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	
-	pass # Replace with function body.
-
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(body: Node2D):
+	if body.is_in_group("Player"):
+		torch.disabled=true
+	if body.is_in_group("enemy"):
+		torch.disabled=false
 	pass # Replace with function body.
