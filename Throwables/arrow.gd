32b72d3cda7ed3ac8fd2b58ca02366e998345102
Hamza Butt <hb979067@gmail.com>
2025-08-02 17:09:57 +0500

    arrow complete

--- Throwables/arrow.gd
+++ Throwables/arrow.gd
@@ -1,29 +1,41 @@
 extends CharacterBody2D
 @onready var Enemy 
 var targetlocation:Vector2
-const SPEED =1
+const SPEED =500
 var isCollided:bool=false
 var checkforCollision=false
 @onready var sprite:Sprite2D=$Sprite2D
-func _ready() :
-	pass
+var dir:Vector2
+func assigndir():
+	dir=targetlocation.normalized()
+func ready():
+	call_deferred(assigndir())
 func _physics_process(delta: float) -> void:
+	dir=targetlocation.normalized()
 	if global_position == targetlocation:
 		print('Pohanch gya')
 	if !isCollided:
-		velocity=targetlocation*SPEED
+		velocity=SPEED*dir
 	else:
 		velocity=Vector2()
 	move_and_slide()
 func _on_hit_body_entered(body: Node2D) -> void:
-	if checkforCollision:
-		isCollided=true
+	#if checkforCollision:
+	
+	
+	if body.is_in_group("Player"):
+		isCollided=false
+		print("wrong collide")
+	elif body.is_in_group('enemy'):
+		body.gotHit()
 		print(Enemy)
-		if body.is_in_group('enemy'):
-			body.gotHit()
-			print("Banda mara")
-		else:
-			print("dewaar")
+		isCollided=true
+		print("Banda mara")
+		print("iscollided elif")
+	else:
+		isCollided=true
+		print("iscollided else")
+		print("dewaar")
 func _on_hit_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		checkforCollision=true
